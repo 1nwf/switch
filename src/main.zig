818e01d6af58a19e579880cd13e105bf69081e7a
Nawaf <nwf.aloufi@gmail.com>
2023-04-16 04:44:47 -0400

    run app in main()

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const App = @import("app.zig").App;
 const Terminal = @import("terminal.zig");
 const DB = @import("db.zig");
 const ansi_term = @import("ansi-term");
@@ -7,27 +8,8 @@ pub fn main() !void {
     var term = try Terminal.init();
     var arena_alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     var alloc = arena_alloc.allocator();
-
     defer arena_alloc.deinit();
-
     var db = try DB.init(alloc);
-    defer db.deinit();
-
-    const entries = try db.read(alloc);
-    for (entries, 0..) |i, idx| {
-        term.writeln("{}. {s}", .{ idx + 1, i });
-    }
-
-    while (true) {
-        const str = try term.read();
-        var val = std.fmt.parseInt(usize, str, 0) catch {
-            continue;
-        };
-        if (val > entries.len or val <= 0) {
-            continue;
-        }
-        term.deinit();
-        try std.io.getStdOut().writeAll(entries[val - 1]);
-        return;
-    }
+    var app = App.init(.num, term, db);
+    try app.run(alloc);
 }
