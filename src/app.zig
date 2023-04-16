15849cc4c5a322806f6ab09790eb34dd950ae6e6
Nawaf <nwf.aloufi@gmail.com>
2023-04-16 04:44:03 -0400

    move app logic to app.zig

---
+++ src/app.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const Terminal = @import("terminal.zig");
+const Mode = enum { num, search };
+const DB = @import("db.zig");
+pub const App = struct {
+    mode: Mode,
+    term: Terminal,
+    db: DB,
+    entries: [][]const u8 = undefined,
+    pub fn init(mode: Mode, term: Terminal, db: DB) App {
+        return App{
+            .mode = mode,
+            .term = term,
+            .db = db,
+        };
+    }
+
+    pub fn run(self: *App, alloc: std.mem.Allocator) !void {
+        defer self.deinit();
+        self.entries = try self.db.read(alloc);
+        for (self.entries) |dir| {
+            _ = self.term.writer.write(dir) catch {};
+            self.term.writeln("", .{});
+        }
+
+        while (true) {
+            const str = try self.term.read();
+            var val = std.fmt.parseInt(usize, str, 0) catch {
+                continue;
+            };
+            if (val > self.entries.len or val <= 0) {
+                continue;
+            }
+            self.term.clear();
+            try std.io.getStdOut().writeAll(self.entries[val - 1]);
+            return;
+        }
+    }
+
+    pub fn deinit(self: *App) void {
+        self.term.deinit();
+        self.db.deinit();
+    }
+};
