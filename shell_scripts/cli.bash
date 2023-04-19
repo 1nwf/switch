21363dbf0c8b89cafa659b4300cee0931ec47bd4
Nawaf <nwf.aloufi@gmail.com>
2023-04-19 06:20:32 -0400

    update shell scripts

--- shell_scripts/cli.bash
+++ shell_scripts/cli.bash
@@ -2,8 +2,13 @@ sw() {
   if [ $# -eq 0 ]
     then
       dir="$(/Users/nwf/Desktop/projects/zig/dir-cli/zig-out/bin/dir-cli)"
-      echo "${dir}"
-      cd "${dir}"
+    if [ -z "$dir" ]
+      then
+        return
+      else
+        echo "${dir}"
+        cd "${dir}"
+    fi
     else 
     /Users/nwf/Desktop/projects/zig/dir-cli/zig-out/bin/dir-cli $*  
   fi
--- shell_scripts/cli.fish
+++ shell_scripts/cli.fish
@@ -3,6 +3,9 @@ function sw
         /Users/nwf/Desktop/projects/zig/dir-cli/zig-out/bin/dir-cli $argv
     else
         set -l output (/Users/nwf/Desktop/projects/zig/dir-cli/zig-out/bin/dir-cli)
+        if test -z $output 
+            return
+        end
         echo $output
         cd $output
   end
